c27e71b6273db35d2977d054919848acb5afd175
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2014-12-27 16:11:00 +0100

    Add gulp with JSHint and JSCS

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp   = require('gulp');
+var gutil   = require('gulp-util');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+var exec = require('child_process').exec;
+
+var scripts = [
+    './**/*.js',
+    '!./node_modules/**/*.js'
+];
+
+gulp.task('lint', function() {
+    return gulp.src(scripts)
+        .pipe(jshint('./.jshintrc'))
+        .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(jshint.reporter('fail'))
+        .pipe(jscs());
+});
+
+gulp.task('test', function(cb) {
+    exec('npm test', function(err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        cb(err);
+    });
+});
+
+gulp.task('default', [ 'lint' ]);
+gulp.task('travis', [ 'lint', 'test' ]);
